c79f72aaf245434b415967f2f45f25e8253898c2
Kaike1-k <Kaikemartinsdasilva1@gmail.com>
2025-12-03 23:43:32 -0300

    final 2

--- Projeto/cadastro.php
+++ Projeto/cadastro.php
@@ -4,15 +4,22 @@
 	//	Recebendo	dados	do	formulário	
 	$nome	=	$_POST['nome'];	
 	$conta	=	$_POST['conta'];	
+	$senha	=   $_POST['senha'];
 	$data	=	$_POST['data'];	
 	$cpf	=	$_POST['cpf'];	
-	
+	$sexo	=   $_POST['sexo'];
+	$nomemae = $_POST['nomemae'];
+	$cep  =  $_POST[' cep'];
+	$endereco  =  $_POST['endereço'];
+	$email  =  $_POST['email'];
+	$telefone  =  $_POST['telefone'];
 	//	Preparando	comando	SQL	
-	$sql	=	"INSERT	INTO clientes (nome,	conta,	data_nascimento, cpf) VALUES	(?,	?,	?,	?)";	
+	$sql	=	"INSERT	INTO users (nome,	conta, senha, data_nascimento, cpf, sexo, nome_mãe, cep, endereço, email, telefone) 
+				 VALUES	(?,	?,	?,	?, ?, ?, ?, ?, ?, ?, ?)";	
 	
 	//	Usando	prepared	statement	para	segurança	
 	$stmt	=	$conexao->prepare($sql);	
-	$stmt->bind_param("ssss",	$nome,	$conta,	$data,	$cpf);	
+	$stmt->bind_param("sssssssssss",	$nome,	$conta, $senha,	$data,	$cpf, $sexo, $nomemae, $cep, $endereco, $email, $telefone);	
 	
 	if	($stmt->execute())	{	
 		echo	"✅cadastrado	com	sucesso!";	
